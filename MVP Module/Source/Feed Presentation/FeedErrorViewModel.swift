d01981e5c6976c32f924ea467071de6d5f59237c
stzn <sztk1209@gmail.com>
2021-05-01 09:19:18 +0900

    MVP - add feed with error snapshot

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Shinzan Takata on 2021/04/30.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+	let message: String?
+}
--- MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -8,8 +8,10 @@ protocol FeedViewControllerDelegate {
 	func didRequestFeedRefresh()
 }
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
 	var delegate: FeedViewControllerDelegate?
+
+	@IBOutlet private var errorView: ErrorView!
 
 	private var tableModel = [FeedImageCellController]() {
 		didSet { tableView.reloadData() }
@@ -35,6 +37,14 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
 	func display(_ cellControllers: [FeedImageCellController]) {
 		tableModel = cellControllers
+	}
+
+	func display(_ viewModel: FeedErrorViewModel) {
+		if let message = viewModel.message {
+			errorView.show(message: message)
+		} else {
+			errorView.hideMessage()
+		}
 	}
 
 	public override func viewDidLayoutSubviews() {
